b5f33700a215bd045b3c31ce4d6d74d63dd9f871
Aynara Bendana <aynarabendana@gmail.com>
2025-02-28 19:19:37 -0600

    version 1 amigo secreto

--- app.js
+++ app.js
@@ -1,3 +1,66 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 //  Aquí deberás desarrollar la lógica para resolver el problema.
-let nombreamiago = 9;
+
+// ATENCION: mi codigo esta en ingles porque estoy aprendiendo el idioma y quiero practicar
+
+let friend = [];
+
+let list = document.getElementById("showFriendList");
+let showResultsHtml = document.getElementById("showFriendlist");
+
+
+function enterFriendName() 
+   {
+     let enterName = document.getElementById("amigo");
+     let friendName = enterName.value;
+
+       if (!friendName)
+         {
+            alert("Please enter a name");
+         return;
+         }
+
+     friend.push(friendName);
+     enterName.value = "";
+     enterName.focus();
+
+     console.log(friend); 
+     
+      let item = document.createElement("li");
+      item.textContent = friendName;
+      list.appendChild(item);
+     
+   }
+
+function showFriendList()
+   {
+     
+   //   list.innerHTML = "";
+   // list.innerHTML = showFriendList();
+
+   //   for (let i = 0; i < friend.length; i++)
+   //     {
+   //       let item = document.createElement("li");
+   //       item.textContent = friend[i];
+   //       list.appendChild(item);
+   //     }
+
+   }
+/*
+   function sortFriendList()
+   {
+     let result = Math.floor (Math.random() * friend.length);
+     let friendResult = (friend [result]);
+     console.log(friendResult);
+
+     showResults();
+   }
+
+   function showResults()
+   {
+      list.innerHTML = "";
+      let item2 = document.createElement("li");
+      item2.textContent = friendResult; 
+      showResultsHtml.appendChild(item2);
+   }
+      */
